04341dd03ed01e3db09d9958dbca842a30a4b7c6
Will Barnard <willbarnard687@pm.me>
2021-06-27 19:10:29 +0100

    Removed usbimager zip, pull from official repo

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,19 +1,13 @@
-﻿$ErrorActionPreference = 'Stop'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName   = 'usbimager.portable'
-  unzipLocation = '$toolsDir\imager.zip'
-  file          = "$toolsDir\imager.zip"
-  Destination   =  $toolsDir
+  unzipLocation = Split-Path $MyInvocation.MyCommand.Definition
+  url           = 'https://gitlab.com/bztsrc/usbimager/raw/binaries/usbimager_1.0.7-i686-win-gdi.zip'
   checksum      = '610CD3853D968E12E83F331F32A8999E'
   checksumType  = 'sha256'
-  SpecificFolder = 'USBImager'
 }
 
-Get-ChocolateyUnzip @packageArgs
-
-Set-Content -Path (Join-Path -Path $toolsDir -ChildPath "usbimager.exe.gui") `
-            -Value $null
-
-Remove-Item -force "$toolsDir\*.zip" -ea 0
+Install-ChocolateyZipPackage @packageArgs
+$File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.zip
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
