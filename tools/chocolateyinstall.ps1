18bc0fcfbf83092f535b46bc1a1ecbc585fc5b7f
Will Barnard <willbarnard687@pm.me>
2021-06-27 15:29:37 +0100

    Initial Commit, removed all nupkg files.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = 'usbimager.portable'
+  unzipLocation = '$toolsDir\imager.zip'
+  file          = "$toolsDir\imager.zip"
+  Destination   =  $toolsDir
+  checksum      = '610CD3853D968E12E83F331F32A8999E'
+  checksumType  = 'sha256'
+  SpecificFolder = 'USBImager'
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Set-Content -Path (Join-Path -Path $toolsDir -ChildPath "usbimager.exe.gui") `
+            -Value $null
+
+Remove-Item -force "$toolsDir\*.zip" -ea 0
