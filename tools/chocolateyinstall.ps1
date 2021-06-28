0872591713e809a4158591a0708520ca8d638710
William Barnard <willbarnard687@pm.me>
2021-06-28 16:50:05 +0100

    Amended to ShimGen binary

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,8 +6,13 @@ $packageArgs = @{
   url           = 'https://gitlab.com/bztsrc/usbimager/raw/binaries/usbimager_1.0.7-i686-win-gdi.zip'
   checksum      = '610CD3853D968E12E83F331F32A8999E'
   checksumType  = 'sha256'
+  Destination   = $toolsDir
 }
 
-Install-ChocolateyZipPackage @packageArgs
-$File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.zip
-Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
+Get-ChocolateyUnzip @packageArgs @unzipLocation
+
+# Workaround for ShimGen GUI, rather than console variant
+Set-Content -Path (Join-Path -Path $toolsDir -ChildPath "usbimager.exe.gui") `
+            -Value $null
+
+Remove-Item -force "$toolsDir\*.zip" -ea 0
